481d02081d52de872ace084a5f239853de55c47f
einfachIrgendwer0815 <eavmb@gmx.de>
2021-06-04 09:53:31 +0200

    optimized code

--- modules/init.py
+++ modules/init.py
@@ -3,44 +3,25 @@ import datetime
 import logging
 import mysql.connector
 
-class config():
-    def __init__(self):
-        with open('config/config.json') as json_file:
-            jsonstructure = json.load(json_file)
-            for p in jsonstructure['discord']:
-                self.token = p['token']
-                self.DBhost = p['DBhost']
-                self.DBuser = p['DBuser']
-                self.DBpasswd = p['DBpasswd']
-                self.DBdatabase = p['DBdatabase']
-                self.IDChannelSpawner = p['IDChannelSpawner']
-                self.IDEmote = p['IDEmote']
-                self.NameEmote = p['NameEmote']
+configs = {}
 
-    def get_token(self):
-        return self.token
-    def get_DBhost(self):
-        return self.DBhost
-    def get_DBuser(self):
-        return self.DBuser
-    def get_DBpasswd(self):
-        return self.DBpasswd
-    def get_DBdatabase(self):
-        return self.DBdatabase
-    def get_IDChannelSpawner(self):
-        return self.IDChannelSpawner
-    def get_IDEmote(self):
-        return self.IDEmote
-    def get_NameEmote(self):
-        return self.NameEmote
+with open('config/config.json') as json_file:
+    jsonstructure = json.load(json_file)
+    for p in jsonstructure['discord']:
+        configs["token"] = p['token']
+        configs["DBhost"] = p['DBhost']
+        configs["DBuser"] = p['DBuser']
+        configs["DBpasswd"] = p['DBpasswd']
+        configs["DBdatabase"] = p['DBdatabase']
+        configs["IDChannelSpawner"] = p['IDChannelSpawner']
+        configs["IDEmote"] = p['IDEmote']
+        configs["NameEmote"] = p['NameEmote']
+
 
 mydb = mysql.connector.connect(
-    host=config().get_DBhost(),
-    user=config().get_DBuser(),
-    passwd=config().get_DBpasswd(),
-    database=config().get_DBdatabase(),
+    host=configs["DBhost"],
+    user=configs["DBuser"],
+    passwd=configs["DBpasswd"],
+    database=configs["DBdatabase"],
     auth_plugin='mysql_native_password'
 )
-
-def getdb():
-    return(mydb)
